c778c70a3704f752c5593a9d3ecb3905005619ed
Graham Wells <gee85@hotmail.co.uk>
2025-01-20 21:02:03 +0000

    Gameplay Effect Delegates

--- Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -3,3 +3,14 @@
 
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 
+void UAuraAbilitySystemComponent::AbilityActorInfoSet()
+{
+	// Bind to delegate from AbilitySystemComponent.h
+	OnGameplayEffectAppliedDelegateToSelf.AddUObject(this, &UAuraAbilitySystemComponent::EffectApplied);
+}
+
+void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
+                                                const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveEffectHandle)
+{
+	GEngine->AddOnScreenDebugMessage(1, 8.f, FColor::Blue, "EffectApplied");
+}
